7747c0be1b50c9e1e28530844f163af660e176b6
TakayukiYamaguchi <taka.radical0921@icloud.com>
2020-06-03 15:25:10 +0900

    add:bot_chatファイルを追加

---
+++ engine/bot_chat.py
@@ -0,0 +1,31 @@
+import chatbot as b
+
+phase = 0
+limit = 3
+first_word = '魔法'
+
+def dialogue(w):
+    global phase, limit
+    try:
+        phase += 1
+        print('phase' + str(phase))
+        word1 = b.load_w2v(w)
+        sentence1 = b.make_sentence(word1)
+        print('bot1:' + sentence1)
+        word2 = b.tokenize(sentence1)
+        sentence2 = b.make_sentence(word2)
+        print('bot2:' + sentence2)
+        next_word_base = b.tokenize(sentence2)
+        if not phase >= limit:
+            dialogue(next_word_base)
+        else:
+            return
+    except Exception as e:
+        print('occur an error.' + e)
+        exit(0)
+def main():
+    dialogue(first_word)
+
+
+if __name__ == '__main__':
+    main()
