b775796ead612f81678b728df80343bef1890983
TakayukiYamaguchi <taka.radical0921@icloud.com>
2020-06-05 00:17:22 +0900

    add:vector.pyを更新

--- engine/vector.py
+++ engine/vector.py
@@ -1,9 +1,12 @@
 import chatbot as b
 
-from keras.preprocessing.text import Tokenizer
+from janome.tokenizer import Tokenizer
+
+from keras.preprocessing.text import Tokenizer as TOKEN
+
 
 phase = 0
-limit = 2
+limit = 3
 first_word = '魔法'
 
 def dialogue(w):
@@ -13,15 +16,22 @@ def dialogue(w):
         print('phase' + str(phase))
         word1 = b.load_w2v(w)
         sentence1 = b.make_sentence(word1)
-        
-        texts = ('bot1:' + sentence1)
-        
-        tokenizer = Tokenizer()
+
+        t = Tokenizer()
+        s = sentence1 
+
+        x = (t.tokenize(s,wakati=True))
+
+        texts = (str(x))
+
+        tokenizer = TOKEN()
         tokenizer.fit_on_texts(texts)
-        
+
         matrix = tokenizer.texts_to_matrix(texts, "tfidf")
         print("bot1:" + str(matrix))
-        
+
+
+
         word2 = b.tokenize(sentence1)
         sentence2 = b.make_sentence(word2)
         print('bot2:' + sentence2)
